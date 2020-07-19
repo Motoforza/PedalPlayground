2efc39b6620f3afc3b23609c1d700f80aaf5d9d3
rkreifeldt <rick@motoforza.net>
2020-07-19 14:09:06 -0600

    Initial Commit

---
+++ PedalPlayground/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PedalPlayground
+//
+//  Created by Rick Kreifeldt on 7/19/20.
+//  Copyright © 2020 ideaBench. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
